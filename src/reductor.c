e4f0208effd0b1764c1de3de0871126c0382b19e
Roddy González <roddy.gonzalez.89@gmail.com>
2022-02-21 21:30:36 -0300

    Reductor: implememnt a function to detect a delta-reductible expression

--- src/reductor.c
+++ src/reductor.c
@@ -4,6 +4,7 @@
 
 #include "wexpression.h"
 #include "wdeclaration.h"
+#include "wfunctions.h"
 #include "reductor.h"
 
 bool is_literal(wexpression_t *expression) {
@@ -23,4 +24,27 @@ bool is_literal(wexpression_t *expression) {
   }
 }
 
-
+bool is_delta_reductible(wexpression_t *expression) {
+  wfunction_t *function;
+  wexpression_t *arg;
+  if (!expression) {
+    return false;
+  }
+  function = wget_function(expression->token);
+  if (!function) {
+    return false;
+  }
+  if (expression->wargc != function->wargc) {
+    return false;
+  }
+  // Check if an argument is a nested expression
+  arg = expression;
+  while(arg) {
+    if (arg->nested_wexpression) {
+      return false;
+    } else {
+      arg = arg->arg_wexpression;
+    }
+  }
+  return true;
+}
--- src/reductor.test.c
+++ src/reductor.test.c
@@ -44,3 +44,61 @@ Test(reductor, should_detect_a_literal) {
   wexpression_free(b);
   wexpression_free(c);
 }
+
+Test(reductor, should_detect_a_delta_reductible_expression) {
+  wexpression_t *plus = wexpression_create(WOPERATOR, "+");
+  wexpression_t *one = wexpression_create(WINTEGER, "1");
+  wexpression_t *two = wexpression_create(WINTEGER, "2");
+  wexpression_append(plus, one);
+  wexpression_append(plus, two);
+  cr_assert(is_delta_reductible(plus));
+  wexpression_free(plus);
+}
+
+Test(reductor, should_not_classify_a_NULL_expression_as_delta_reductible) {
+  cr_assert(! is_delta_reductible(NULL));
+}
+
+Test(reductor, should_not_classify_a_nonexisting_operator_as_delta_reductible) {
+  wexpression_t *plus = wexpression_create(WOPERATOR, "+asdf");
+  wexpression_t *one = wexpression_create(WINTEGER, "1");
+  wexpression_t *two = wexpression_create(WINTEGER, "2");
+  wexpression_append(plus, one);
+  wexpression_append(plus, two);
+  cr_assert(! is_delta_reductible(plus));
+  wexpression_free(plus);
+}
+
+Test(reductor, should_not_classify_a_expression_with_a_nested_one_as_delta_reductible) {
+  wexpression_t *plus = wexpression_create(WOPERATOR, "+");
+  wexpression_t *plus2 = wexpression_create(WOPERATOR, "+");
+  wexpression_t *parenthesis = wexpression_create(WUNKNOWN, "(");
+  wexpression_t *one = wexpression_create(WINTEGER, "1");
+  wexpression_t *two = wexpression_create(WINTEGER, "2");
+  wexpression_t *three = wexpression_create(WINTEGER, "3");
+  // create outter expression
+  wexpression_append(plus, one);
+  wexpression_append(plus, parenthesis);
+  // create inner expression
+  wexpression_append(plus2, two);
+  wexpression_append(plus2, three);
+  // nest expressions
+  wexpression_nest(parenthesis, plus2);
+  // outter expression is not delta-reductible
+  cr_assert(! is_delta_reductible(plus));
+  // inner expression is
+  cr_assert(is_delta_reductible(plus2));
+  wexpression_free(plus);
+}
+
+Test(reductor, should_not_classify_a_expression_with_wrong_number_of_arguments_as_delta_reductible) {
+  wexpression_t *plus = wexpression_create(WOPERATOR, "+");
+  wexpression_t *one = wexpression_create(WINTEGER, "1");
+  wexpression_t *two = wexpression_create(WINTEGER, "2");
+  wexpression_t *three = wexpression_create(WINTEGER, "3");
+  wexpression_append(plus, one);
+  wexpression_append(plus, two);
+  wexpression_append(plus, three);
+  cr_assert(! is_delta_reductible(plus));
+  wexpression_free(plus);
+}
